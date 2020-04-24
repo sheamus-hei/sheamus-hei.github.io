08de2ef54058ba14b277ff63e6214f0dea9477cc
deanjheikkila <deanjheikkila@gmail.com>
2020-04-23 20:07:35 -0700

    fixed links

--- src/App.js
+++ src/App.js
@@ -1,70 +1,15 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import Header from './content/Header';
-import Nav from './content/Nav';
-import About from './content/About';
-import Project from './content/Project';
-import Skills from './content/Skills';
-import Contact from './content/Contact';
+
+import Home from './Home';
 import {HashRouter as Router} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 function App() {
-  let projects = [
-    {
-      name: "Frogcraft",
-      description: "You’re a little frog in a big world, and the rest is up to you! In this 2D platformer, hop around and flick your tongue manipulate terrain.",
-      tech: "Javascript, CSS, HTML5 Canvas",
-      link: "https://erik-hei.github.io/frogcraft/",
-      github: "https://github.com/erik-hei/frogcraft",
-      img: "./frogcraft.png",
-      gif: "./frogcraft.gif"
-    },{
-      name: "NatureFinder",
-      description: "Looking for wildlife in your area? NatureFinder allows you to search for any location worldwide and see animals that were spotted recently nearby.",
-      tech: "Express.js, Node.js, PostGreSQL(Sequelize), EJS, HTML, CSS, Bootstrap, Mapbox",
-      link: "http://naturefinder.herokuapp.com/",
-      github: "https://github.com/erik-hei/nature-finder",
-      img: "./naturefinder.png",
-      gif: "./naturefinder.gif"
-    },{
-      name: "No Scroll Recipes",
-      description: "Are you tired of scrolling past a food blogger’s life story to get to their recipe? This app is for you. No Scroll is a micro-blogging platform for recipes that limits recipe descriptions to the length of a tweet.",
-      tech: "MERN - MongoDB (Mongoose), Express.js, React, Node.js, CSS",
-      link: "https://recipe-app-29.herokuapp.com/",
-      github: "https://github.com/erik-hei/recipe-app-project3",
-      img: "./noscroll.png",
-      gif: "./noscroll.gif"
-    },{
-      name: "Lyrical",
-      description: "Now you can create poetry using your favorite song lyrics. Make new poems and then search lyrics from Genius to add fresh phrases to your creations.",
-      quote: "“This app sparks joy.” - Sarah King, GA Instructor",
-      tech: "Flask (Python), React, PostGreSQL (SQLALchemy), Material-UI, CSS",
-      link: "https://erik-hei.github.io/lyrical/",
-      github: "https://github.com/erik-hei/lyrical",
-      img: "./lyrical.png",
-      gif: "./lyrical.png"
-    }
-  ]
-
   return (
     <Router>
-    <div className="App">
-      <header>
-        <Header className="header" />
-        <Nav />
-      </header>
-      <main>
-        <About id="about" />
-        <h1 className="fancy proj-title" id="projects">Projects</h1>
-        <Project project={projects[3]} />
-        <Project project={projects[2]} />
-        <Project project={projects[1]} />
-        <Project project={projects[0]} />
-        <Skills id="skills" />
-      </main>
-      <Contact id="contact" />
-    </div>
+      <Route exact path='/' component={Home} />
     </Router>
   );
 }
--- src/content/Nav.js
+++ src/content/Nav.js
@@ -8,9 +8,9 @@ export default function Nav(props) {
         <Slide direction="up" in="true" timeout={{enter:2000}}>
             <nav>
                 <ul>
-                    <Link to="#about"><li>About</li></Link>
-                    <Link to="#projects"><li>Projects</li></Link>
-                    <Link to="#skills"><li>Skills</li></Link>
+                    <Link to="/#about"><li>About</li></Link>
+                    <Link to="/#projects"><li>Projects</li></Link>
+                    <Link to="/#skills"><li>Skills</li></Link>
                     <Link to="/#contact"><li>Contact & Resume</li></Link>
                 </ul>
             </nav>
