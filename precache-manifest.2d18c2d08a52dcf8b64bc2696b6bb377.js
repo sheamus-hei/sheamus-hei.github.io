954862d58d12cc0507e9422b89bf6bfe786c8299
deanjheikkila <deanjheikkila@gmail.com>
2020-04-23 20:21:39 -0700

    Updates

---
+++ precache-manifest.2d18c2d08a52dcf8b64bc2696b6bb377.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "490534e50efa712dae0aadaf2e50e4f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b34236dab2a1b2b50cc9",
+    "url": "/static/css/main.8b224bce.chunk.css"
+  },
+  {
+    "revision": "721efe45461c6a59fdf5",
+    "url": "/static/js/2.85c2912f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85c2912f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b34236dab2a1b2b50cc9",
+    "url": "/static/js/main.d264d53b.chunk.js"
+  },
+  {
+    "revision": "ed67f50a89a7b8e60af9",
+    "url": "/static/js/runtime-main.97b86218.js"
+  }
+]);
