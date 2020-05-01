265d4ad2eceb0d1c7bbc4b95a8b88ff06258d407
deanjheikkila <deanjheikkila@gmail.com>
2020-04-30 21:57:13 -0700

    redeploy

---
+++ README.md
@@ -0,0 +1,14 @@
+## Erik's Portfolio
+
+[See it live here](https://erik-hei.github.io/#/)
+
+### Tech Used
+* React.js
+* Material-UI (for animations)
+* Octocons
+
+
+![](./city.jpg)
+
+Copyright © 2020 Erik (dj) Heikkila
+
---
+++ precache-manifest.cd488b60acb8a715eeb34ed06394d0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d95192c3903940152cf751c3ef68c8f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d07e32ee4a93d50e1611",
+    "url": "/static/css/main.1261db8d.chunk.css"
+  },
+  {
+    "revision": "721efe45461c6a59fdf5",
+    "url": "/static/js/2.85c2912f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.85c2912f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d07e32ee4a93d50e1611",
+    "url": "/static/js/main.7f1f4f79.chunk.js"
+  },
+  {
+    "revision": "ed67f50a89a7b8e60af9",
+    "url": "/static/js/runtime-main.97b86218.js"
+  }
+]);
